dd54ad93b1df49cc3b53cde2ca309eccfd41df81
Carlos Hoyos <carlos.edhr@comunidad.unam.mx>
2020-01-21 13:51:52 -0800

    async calls to Petfinder API, Pet component updated

--- src/SearchParams.js
+++ src/SearchParams.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import pet, { ANIMALS } from "@frontendmasters/pet";
+import Results from "./Results";
 import useDropdown from "./useDropdown";
 
 const SearchParams = () => {
@@ -53,6 +54,7 @@ const SearchParams = () => {
 
         <button>Submit</button>
       </form>
+      <Results pets={pets} />
     </div>
   );
 };
